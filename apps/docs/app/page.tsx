4a28021c522c689433282a6006bf99f87c228c74
Sadiq Gurramkonda <gurramkondasadiq216@gmail.com>
2026-07-16 19:46:01 +0530

    add pre-commit hooks

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
@@ -6,20 +6,27 @@ import styles from "./page.module.css";
 import { Card } from "@repo/ui/card";
 import { useState } from "react";
 
-
 export default function Home() {
-  const [num1,setNum1] = useState<number>(0);
-  const [num2,setNum2] = useState<number>(0);
-  const sum = num1+num2;
+  const [num1, setNum1] = useState<number>(0);
+  const [num2, setNum2] = useState<number>(0);
+  const sum = num1 + num2;
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <h3>calculate sum</h3>
-        <div>
-          the sum is here: {num1+num2}
-        </div>
-        <input onChange={(e)=>{setNum1(Number(e.target.value) )}} type="number"></input>
-        <input onChange={(e)=>{setNum2(Number(e.target.value) )}} type="number"></input>
+        <div>the sum is here: {num1 + num2}</div>
+        <input
+          onChange={(e) => {
+            setNum1(Number(e.target.value));
+          }}
+          type="number"
+        ></input>
+        <input
+          onChange={(e) => {
+            setNum2(Number(e.target.value));
+          }}
+          type="number"
+        ></input>
       </main>
     </div>
   );
